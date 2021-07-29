2350dd64f5f2f303f46b2aee9ca795ffb4bdfaad
Justin Byrne <theskaterjesus@gmail.com>
2021-07-29 22:17:31 +0100

    Completed the challenge

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkCyan: 'hsl(185, 75%, 39%)',
+        darkBlue: 'hsl(229, 23%, 23%)',
+        grayBlue: 'hsl(227, 10%, 46%)',
+        darkGray: 'hsl(0, 0%, 59%)',
+      },
+      fontFamily: {
+        kumbh: 'Kumbh Sans, sans-serif',
+      },
+    },
   },
   variants: {
     extend: {},
